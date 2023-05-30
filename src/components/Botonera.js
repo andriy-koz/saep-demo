b77f3c8ccdbd035419a6bd4a74c942a63f303ed3
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-05-30 18:40:43 -0300

    Botonera y EntradasYSalidas

--- src/components/Botonera.js
+++ src/components/Botonera.js
@@ -1,5 +1,23 @@
 const Botonera = () => {
-  return <div>Botonera</div>;
+  return (
+    <div className='botonera-contenedor'>
+      <h3>Botonera</h3>
+      <div className='botonera'>
+        <div className='botonera-boton'>
+          <h4>INICIO</h4>
+          <div className='btn-inicio'></div>
+        </div>
+        <div className='botonera-boton'>
+          <h4>PARADA</h4>
+          <div className='btn-parada'></div>
+        </div>
+        <div className='botonera-boton'>
+          <h4>CALIBRAR</h4>
+          <div className='btn-calibrar'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Botonera;
